11faecff36bd4b87a611ebe3392faf5c0759aa0d
Enrique Pérez <psychgeometry@gmail.com>
2015-09-13 00:33:15 +0200

    elemets

--- docs/tbexcel/csv/EXPLOCAL.R
+++ docs/tbexcel/csv/EXPLOCAL.R
@@ -1,7 +1,7 @@
 library(dplyr)
 library(tidyr)
 library(ggplot2)
-
+library(latex2exp)
 
 entalp.file <- "ENTALP.CSV"
 
@@ -21,6 +21,11 @@ dioxides <- filter(explosion.products,
                    )
 
 sesquioxides <- filter(explosion.products,  grepl("2O3", FORMULA))
+
+#elemental <- filter(explosion.products,  FORMULA %in% c("C", "Hg"))
+
+elements <- data.frame(FORMULA = c("C", "Hg"))
+
 
 entalp.data <- entalp.data %>%
     gather(key = FORMULA, value = ENTHALPY, -TEMP)
@@ -53,28 +58,44 @@ ggplot(sesquioxides.entalp.data, aes(TEMP , ENTHALPY, col = FORMULA)) +
     ylab("H(T)-H(298) kcal/mol") +
     geom_line()
 
+elements.entalp.data <- entalp.data %>%
+    filter(FORMULA %in% elements$FORMULA)
 
+ggplot(elements.entalp.data, aes(TEMP , ENTHALPY, col = FORMULA)) +
+    ggtitle("Elements") +
+    xlab("Temperature (K)") +
+    ylab("H(T)-H(298) kcal/mol") +
+    geom_line()
 
 # Constants data
 # File K1.csv
 k1.file <- "K1.CSV"
 # Temperatura (K)
-# K1= PCO.PH2O / PCO2·PH2
+# K1=PCO.PH2O/PCO2·PH2
 # K2=P²CO/PCO2
 
 k1.data <- read.csv(k1.file,
                     dec = ",",
                     sep = ";",
-                    col.names = c("T", "K1", "K2"),
+                    col.names = c("TEMP", "K1", "K2"),
                     skip = 1
                     )
 
 
-View(k1.data)
+label.k1.plot <- latex2exp("$K_{1} = P_{CO} . P_{H_{2}O}/P_{CO_{2}} . P_{H_{2}}$")
 
-ggplot(k1.data, aes(x = T, y = K1)) +
+
+ggplot(k1.data, aes(x = TEMP, y = K1)) +
+    xlab("Temperature (K)") +
+    ylab(label.k1.plot) +
+    annotate("text", x = 1500, y = 7.5, label = "frac(P[CO], P[H2O]) * frac(P[CO2], P[H2])" , parse = TRUE) + 
+    geom_line()
+    
+
+label.k2.plot <- latex2exp("$K_{2}= P_{CO}^2/P_{CO_2}$")
+
+ggplot(k1.data, aes(x = TEMP, y = K2)) +
+    xlab("Temperature (K)") +
+    ylab(label.k2.plot) +
     geom_line()
 
-
-ggplot(k1.data, aes(x = T, y = K2)) +
-    geom_line()
